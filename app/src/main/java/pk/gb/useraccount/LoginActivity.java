6c288341bc08337fd44b7e3d5a1b5df4d8c0dcf7
ZalfaFirdaus <zalfafirdaus6@gmail.com>
2020-05-02 00:41:24 +0700

    login

---
+++ app/src/main/java/pk/gb/useraccount/LoginActivity.java
@@ -0,0 +1,26 @@
+package pk.gb.useraccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        img = findViewById(R.id.GoBackIcon);
+    }
+
+    public void goback(View view) {
+        Intent ic = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(ic);
+    }
+}
